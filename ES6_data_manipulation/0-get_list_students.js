0da477792ea2ba424901060d3c9ca38a615f5a47
binbinher <binbin.he515@outlook.com>
2024-11-27 20:48:55 +0100

    update 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,13 @@
-function getListStudents() {
-	return [
+// returns a list of students
+
+const getListStudents = () => {
+	const students = [
 		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
 		{ id: 2, firstName: 'James', location: 'Columbia' },
 		{ id: 5, firstName: 'Serena', location: 'San Francisco' },
 	];
-}
+
+	return students;
+};
 
 export default getListStudents;
