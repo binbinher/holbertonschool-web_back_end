225242dedfc7f8a19268c2e3a4668fa6bb193fbf
binbinher <binbin.he515@outlook.com>
2024-11-29 17:28:11 +0100

    fix eslint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,23 +1,23 @@
 // returns a string of all the set values that start with a specific string (startString)
 
 const cleanSet = (set, startString) => {
-	const str = [];
+  const str = [];
 
-	if (
-		typeof set !== 'object'
+  if (
+    typeof set !== 'object'
 		|| typeof startString !== 'string'
 		|| startString.length === 0
-	) {
-		return '';
-	}
+  ) {
+    return '';
+  }
 
-	for (const item of set) {
-		if (item && item.startsWith(startString)) {
-			str.push(item.slice(startString.length));
-		}
-	}
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      str.push(item.slice(startString.length));
+    }
+  }
 
-	return str.join('-');
+  return str.join('-');
 };
 
 export default cleanSet;
