aec57975e9e52c02484f4cbc79b19bb15e23560a
binbinher <binbin.he515@outlook.com>
2024-11-29 19:02:06 +0100

    fix eslint

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,7 +1,7 @@
 // return a rejected promise with the error message: `${filename} cannot be processed`
 
 export default function uploadPhoto(filename) {
-	return Promise.reject(
-		new Error(`${filename} cannot be processed`),
-	);
+  return Promise.reject(
+    new Error(`${filename} cannot be processed`),
+  );
 }
