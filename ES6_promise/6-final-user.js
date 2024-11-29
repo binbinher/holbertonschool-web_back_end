498af9f2e64324dbd9b58099d96133c260dced02
binbinher <binbin.he515@outlook.com>
2024-11-29 19:10:42 +0100

    fix eslint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,11 +4,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-	return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-		.then((results) => results.map((result) => ({
-			status: result.status,
-			value: result.status === 'fulfilled' ? result.value : result.reason,
-		})));
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
 
 export default handleProfileSignup;
--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 // return the promise that resolves first
 
 export default function loadBalancer(chinaDownload, USDownload) {
-	return Promise.race([chinaDownload, USDownload]);
+  return Promise.race([chinaDownload, USDownload]);
 }
