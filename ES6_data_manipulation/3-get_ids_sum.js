9c9086b378764e36807f770f74925ce8ceeab306
binbinher <binbin.he515@outlook.com>
2024-11-29 10:36:31 +0100

    delete repo

--- ES6_data_manipulation/3-get_ids_sum.js
+++
@@ -1,5 +0,0 @@
-// return the sum of all the student ids
-
-const getStudentIdsSum = (students) => students.reduce((sum, student) => sum + student.id, 0);
-
-export default getStudentIdsSum;
--- ES6_data_manipulation/6-set.js
+++
@@ -1,5 +0,0 @@
-// returns a Set from an array
-
-const setFromArray = (data) => new Set(data);
-
-export default setFromArray;
