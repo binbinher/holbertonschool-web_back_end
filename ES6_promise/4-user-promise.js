bc04ed5f86758089bfbb3bcb0a6dfd5cfa8803d7
binbinher <binbin.he515@outlook.com>
2024-11-29 19:00:14 +0100

    add 4n5

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,12 @@
+// return a promise that resolves with an object of user data
+
+function signUpUser(firstName, lastName) {
+	return Promise.resolve(
+		{
+			firstName,
+			lastName,
+		},
+	);
+}
+
+export default signUpUser;
---
+++ ES6_promise/5-photo-reject.js
@@ -0,0 +1,7 @@
+// return a rejected promise with the error message: `${filename} cannot be processed`
+
+export default function uploadPhoto(filename) {
+	return Promise.reject(
+		new Error(`${filename} cannot be processed`),
+	);
+}
