36c043ef79b74a7743d8db41d23a6227a26c1eaf
binbinher <binbin.he515@outlook.com>
2024-11-29 10:42:49 +0100

    correct

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,17 @@
+// Update the value of the key in the map to 100 if the original value is 1
+
+const updateUniqueItems = (map) => {
+	if (!(map instanceof Map)) {
+		throw new Error('Cannot process');
+	}
+
+	for (const [key, value] of map) {
+		if (value === 1) {
+			map.set(key, 100);
+		}
+	}
+
+	return map;
+};
+
+export default updateUniqueItems;
---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,5 @@
+// returns a Set from an array
+
+const setFromArray = (data) => new Set(data);
+
+export default setFromArray;
