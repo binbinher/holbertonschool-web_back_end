778587d36895995aab1c504b533126b81b250b87
binbinher <binbin.he515@outlook.com>
2024-11-29 17:31:19 +0100

    update

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,23 +1,16 @@
 // returns a string of all the set values that start with a specific string (startString)
-
-const cleanSet = (set, startString) => {
-  const str = [];
-
-  if (
-    typeof set !== 'object'
-		|| typeof startString !== 'string'
-		|| startString.length === 0
-  ) {
+export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString === '') {
     return '';
   }
 
-  for (const item of set) {
-    if (item && item.startsWith(startString)) {
-      str.push(item.slice(startString.length));
+  const result = [];
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
     }
   }
 
-  return str.join('-');
-};
-
-export default cleanSet;
+  return result.join('-');
+}
