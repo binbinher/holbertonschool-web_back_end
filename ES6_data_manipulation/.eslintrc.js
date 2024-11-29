98566455d085e444f65c0418fec2950b5a380cee
binbinher <binbin.he515@outlook.com>
2024-11-29 17:17:38 +0100

    fix eslint configaration

--- ES6_data_manipulation/.eslintrc.js
+++ ES6_data_manipulation/.eslintrc.js
@@ -18,20 +18,22 @@ module.exports = {
 	},
 	plugins: ['jest'],
 	rules: {
-		'max-classes-per-file': 'off',
-		'no-underscore-dangle': 'off',
-		'no-console': 'off',
-		'no-shadow': 'off',
+		'max-classes-per-file': 'off', // Allow multiple classes per file
+		'no-underscore-dangle': 'off', // Disable restriction on underscores
+		'no-console': 'off', // Allow console statements
+		'no-shadow': 'off', // Allow variable shadowing
 		'no-restricted-syntax': [
 			'error',
 			'LabeledStatement',
 			'WithStatement',
-		],
+		], // Disallow labeled and with statements
+		'no-tabs': 'error', // Disallow tab characters
+		indent: ['error', 2], // Enforce 2 spaces for indentation
 	},
 	overrides: [
 		{
-			files: ['*.js'],
-			excludedFiles: 'babel.config.js',
-		}
-	]
+			files: ['*.js'], // Apply rules to JavaScript files
+			excludedFiles: ['babel.config.js'], // Exclude babel.config.js explicitly
+		},
+	],
 };
--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,13 @@
 // returns a list of students
 
 const getListStudents = () => {
-	const students = [
-		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-		{ id: 2, firstName: 'James', location: 'Columbia' },
-		{ id: 5, firstName: 'Serena', location: 'San Francisco' },
-	];
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 
-	return students;
+  return students;
 };
 
 export default getListStudents;
