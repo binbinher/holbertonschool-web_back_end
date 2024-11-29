f690a0630f1b452814fb58d31fe60240f2ff0168
binbinher <binbin.he515@outlook.com>
2024-11-29 18:09:56 +0100

    fix eslint

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,14 +1,14 @@
 // return a promise
 
 function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		const success = true;
-		if (success) {
-			resolve();
-		} else {
-			reject();
-		}
-	});
+  return new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
 
 export default getResponseFromAPI;
