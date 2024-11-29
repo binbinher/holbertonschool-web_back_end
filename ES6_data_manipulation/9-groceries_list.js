0ccdf681c54f0f2a6c0c342f1d9ee36b475b9bae
binbinher <binbin.he515@outlook.com>
2024-11-29 17:24:19 +0100

    fix elint

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,15 +1,15 @@
 // list of groceries
 
 const groceriesList = () => {
-	const fruits = new Map([
-		['Apples', 10],
-		['Tomatoes', 10],
-		['Pasta', 1],
-		['Rice', 1],
-		['Banana', 5],
-	]);
+  const fruits = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
 
-	return fruits;
+  return fruits;
 };
 
 export default groceriesList;
