ebabbfb5cd9bb7ffabac9a09d426cbde4bac48a4
binbinher <binbin.he515@outlook.com>
2024-12-05 14:57:09 +0100

    add tsk2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""basic annotation floor"""
+
+import math
+
+def floor(n: float) -> int:
+    """
+    type_annotated function take a float and return a float
+    """
+    return math.floor(n)
