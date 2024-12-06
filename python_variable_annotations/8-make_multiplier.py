bafa2bc1fe0930c1e5dee8babb42b218c916f055
binbinher <binbin.he515@outlook.com>
2024-12-06 21:03:38 +0100

    add tsk8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" complex annotation"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """type-annotated function that takes a float multiplier
+    and returns a function that multiplies a float by multiplier."""
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+    return multiplier_function
